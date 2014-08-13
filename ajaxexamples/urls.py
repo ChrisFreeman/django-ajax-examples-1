584f0190c21c88dbbf1d49aa04d93c3baeab0ca0
Chris Freeman <chris.freeman.pdx@gmail.com>
2014-08-13 11:53:59 -0700

    Update ajax and view function names

--- ajaxexamples/urls.py
+++ ajaxexamples/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'jqtest.views.home', name='home'),
     url(r'^jq_example1/$', 'jqtest.views.jq_example1', name='jq_example1'),
-    url(r'^jq_example2/$', 'jqtest.views.jq_example2', name='jq_example2'),
-    url(r'^jq_example2_submit/$', 'jqtest.views.jq_example2_submit', name='jq_example2'),
+    url(r'^jq_example2_edit_text/$', 'jqtest.views.jq_example2_edit_text', name='jq_example2_edit'),
+    url(r'^jq_example2_submit/$', 'jqtest.views.jq_example2_submit', name='jq_example2_submit'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
